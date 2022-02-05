82e6406a7467fdee34a04bf138981a05eac22608
Deathstalker <iamsoumyass@gmail.com>
2022-02-05 14:57:44 +0530

    finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const timeEl = document.querySelector('#time')
 const scoreEl = document.querySelector('#score')
+const wordEl = document.querySelector('#word')
 const inputEl = document.querySelector('#text')
 const endGameEl = document.querySelector('#end-game-container')
 const settingsBtn = document.querySelector('#settings-btn')
@@ -29,10 +30,70 @@ const words = [
   'loving'
 ];
 
+let word
 let time = 10
 let score = 0
 let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium'
 
 difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium'
 
-console.log(inputEl)
+const gameOver = () => {
+  document.querySelector('.container').innerHTML = ''
+  endGameEl.innerHTML = `
+  <h1>Time ran out!</h1>
+  <p>Your final score is ${score}</p>
+  <button class='reload-btn' onclick="location.reload()">Reload</button>
+  `
+  endGameEl.style.display = 'flex'
+}
+
+const updateTime = () => {
+  time--
+  timeEl.innerText = `${time}s`
+  if(time === 0) {
+    clearInterval(timer)
+    gameOver()
+  }
+}
+
+const timer = setInterval(updateTime, 1000)
+
+const addWordDOM = () => {
+  word = words[Math.floor(Math.random() * words.length)]
+  wordEl.innerText = word
+}
+
+const updateScore = () => {
+  score++
+  scoreEl.innerText = score
+}
+
+addWordDOM()
+
+inputEl.addEventListener('input', (e) => {
+  const text = e.target.value
+  if(text === word) {
+    addWordDOM()
+    updateScore()
+    e.target.value = ''
+
+    if(difficulty === 'easy') {
+      time += 5
+    } else if(difficulty === 'medium') {
+      time += 3
+    } else {
+      time += 2
+    }
+
+    updateTime()
+  }
+})
+
+difficultySelect.addEventListener('change', (e) => {
+  difficulty = e.target.value
+  localStorage.setItem('difficulty', JSON.stringify(difficulty))
+})
+
+settingsBtn.addEventListener('click', () => {
+  document.querySelector('.settings').classList.toggle('hide')
+})
